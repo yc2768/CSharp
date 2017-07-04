74afeb5c6b6f1e2ff242465992697d0dc7686099
Aaron Kong <kongyajie0704@gmail.com>
2017-07-04 16:24:38 +0800

    C#学习

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuadLibrary;
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quad q = new Quad(10, 10);
+            double area = q.GetArea();
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ LearnCSharp/QuadLibrary/Quad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadLibrary
+{
+    public class Quad
+    {
+        private double width = 0;
+        private double height = 0;
+
+        public  Quad(double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+        public double GetArea()
+        {
+            return width * height;
+        }
+    }
+}
