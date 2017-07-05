9f42a98b489a5747e84d7d228bf20fc60054d8ab
yc2768 <2768647687@qq.com>
2017-07-05 14:53:03 +0800

    测试抽象方法在子类继承实现

---
+++ LearnCSharp/Person2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    class Person2
+    {
+        private int age;
+        private string name;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public abstract void ShowInfo();
+    }
+
+}
---
+++ LearnCSharp/Student2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    class Student2:Person2
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("age:{0},name:{1}",Age,Name);
+        }
+    }
+}
